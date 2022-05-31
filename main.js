85a5dc334ad1e0ee4c51631a35077e6ef7bd41cd
Ariel Rojas <ariel.rojas@oxfordcomputergroup.com>
2022-05-31 10:59:31 -0600

    Added animation and new model

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import * as THREE from 'three';
 
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1,1000);
@@ -14,42 +16,47 @@ const renderer = new THREE.WebGL1Renderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+
 
 renderer.render(scene, camera);
 
 //model creation
 
-//ring
-const geometry = new THREE.TorusGeometry(10,3,16,100);
-const material = new THREE.MeshStandardMaterial({color:0xFF6347});
-const torus = new THREE.Mesh(geometry, material);
 
-//cube with img texture
-const jeffTexture = new THREE.TextureLoader().load('ariel.jpg');
-const jeff = new THREE.Mesh( 
-  new THREE.BoxGeometry(3,3,3),
-  new THREE.MeshBasicMaterial({map:jeffTexture})
-)
+//model addition
+const loader = new GLTFLoader();
 
-jeff.position.z = -5;
-jeff.position.x = 2;
+loader.load(
+	// resource URL
+	'scene.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
 
-//moon with normal texture
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+		scene.add( gltf.scene );
 
-const moon = new THREE.Mesh( 
-  new THREE.SphereGeometry(3,32,32),
-  new THREE.MeshStandardMaterial(
-    {
-      map:moonTexture,
-      normalMap:normalTexture
-    }));
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
 
-moon.position.z = 30;
-moon.position.setX(-10);
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+
+
 
 //light creation
 const ambientLight = new THREE.AmbientLight(0xffffff);
@@ -62,57 +69,26 @@ pointLight.position.set(5,5,5);
 
 //const controls = new OrbitControls(camera, renderer.domElement);
 
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25,24,24);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
-  const star = new THREE.Mesh(geometry, material);
-
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-  star.position.set(x,y,z);
-  scene.add(star);
-}
 
 //add to scene
-scene.add(torus);
 scene.add(ambientLight);
 scene.add(pointLight);
 //scene.add(lightHelper);
 //scene.add(gridHelper);
-scene.add(jeff);
-scene.add(moon);
-//Adds 200 stars to the scene
-Array(200).fill().forEach(addStar);
 
 //background
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-scene.background = spaceTexture;
+//const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+//scene.background = spaceTexture;
 
 
-//Scroll Animation
-function moveCamera(){
-  const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
-
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
-
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.position.y = t * -0.0002;
-
-}
-
-document.body.onscroll = moveCamera;
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set(5, 2, -3);
+controls.autoRotate = true;
 
 function animate() {
   requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
 
-  //controls.update();
+  controls.update();
 
   renderer.render(scene, camera);
 }
