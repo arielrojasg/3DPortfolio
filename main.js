ccb3138e39e5b8d31c46f3f726dfa1d1ea5cbd38
arielrojasg18 <arielrojasg18@gmail.com>
2023-01-23 14:33:06 -0600

    removed ssl enforce

--- main.js
+++ main.js
@@ -6,19 +6,7 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 
-var express = require('express');
-var http = require('http');
-var enforce = require('express-sslify');
- 
-var app = express();
- 
-// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
-// a load balancer (e.g. Heroku). See further comments below
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
- 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+
 
 const scene = new THREE.Scene();
 
